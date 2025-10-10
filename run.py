3c8b3c66f877666e5688332e63a3222d600a3950
huao <aohustudy@163.com>
2025-10-10 11:07:41 +0800

    new

--- run.py
+++ run.py
@@ -74,10 +74,22 @@ if __name__ == '__main__':
     parser.add_argument('--devices', type=str, default='0,1,2,3', help='device ids of multile gpus')
 
     # TimeCNN
-    parser.add_argument('--conv_dropout', type=float, default=0, help='conv_dropout')
+    parser.add_argument('--conv_dropout', type=float, default=0.0, help='conv_dropout')
     parser.add_argument('--use_norm', type=int, default=True, help='use norm and denorm')
     parser.add_argument('--inverse', action='store_true', help='inverse output data', default=False)
-
+    
+    # TimeMixer
+    parser.add_argument('--down_sampling_layers', type=int, default=0, help='num of down sampling layers')
+    parser.add_argument('--down_sampling_window', type=int, default=1, help='down sampling window size')
+    parser.add_argument('--down_sampling_method', type=str, default=None,
+                        help='down sampling method, only support avg, max, conv')
+    parser.add_argument('--channel_independence', type=int, default=1,
+                        help='0: channel dependence 1: channel independence for FreTS model')
+    parser.add_argument('--decomp_method', type=str, default='moving_avg',
+                        help='method of series decompsition, only support moving_avg or dft_decomp')
+    
+    # PatchTST
+    parser.add_argument('--pct_start', type=float, default=0.3, help='pct_start')
 
 
     args = parser.parse_args()
